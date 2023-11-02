ebafd63ff97ba0ceafe53d3ccda8bbcac68a63da
TheBoss2004 <147054361+TheBoss2004@users.noreply.github.com>
2023-11-02 13:03:59 +0100

    k

--- docs/script.js
+++ docs/script.js
@@ -1,45 +1,44 @@
-// Sketch.js
-let canvas;
-let ball;
-
 function setup() {
- canvas = createCanvas(windowWidth, windowHeight, WEBGL);
- ball = new Ball();
-}
-
-function draw() {
- background(0);
- rotateX(frameCount * 0.01);
- rotateY(frameCount * 0.01);
- rotateZ(frameCount * 0.01);
-
- ball.update();
- ball.display();
-}
-
-// Ball.js
-class Ball {
- constructor() {
-    this.position = createVector(0, 0, 0);
-    this.velocity = createVector(0, 0, 0);
-    this.acceleration = createVector(0, 0, 0);
- }
-
- update() {
-    const windowSize = min(windowWidth, windowHeight);
-    const force = p5.Vector.random3D();
-    force.mult(windowSize * 0.001);
-
-    this.acceleration.add(force);
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-
-    this.acceleration.mult(0);
- }
-
- display() {
-    translate(this.position.x, this.position.y, this.position.z);
-    fill(255, 0, 0);
-    sphere(20);
- }
-}
+    createCanvas(400, 400, WEBGL);
+    // This enables device orientation for your application.
+    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+       DeviceOrientationEvent.requestPermission()
+         .then(response => {
+           if (response === 'granted') {
+             window.addEventListener('deviceorientation', deviceOrientationHandler, true);
+           }
+         })
+         .catch(console.error);
+    } else {
+       window.addEventListener('deviceorientation', deviceOrientationHandler, true);
+    }
+   }
+   
+   function draw() {
+    background(255);
+    translate(-width / 2, -height / 2);
+   
+    rotateX(ball.roll * PI / 180);
+    rotateY(ball.pitch * PI / 180);
+    rotateZ(ball.yaw * PI / 180);
+   
+    noStroke();
+    fill(0, 0, 255);
+    sphere(100);
+   }
+   
+   let ball = {
+    roll: 0,
+    pitch: 0,
+    yaw: 0
+   };
+   
+   function deviceOrientationHandler(event) {
+    if (event.beta !== null && event.gamma !== null) {
+      ball.roll = event.beta;
+      ball.pitch = event.gamma;
+    }
+    if (event.alpha !== null) {
+      ball.yaw = event.alpha;
+    }
+   }
