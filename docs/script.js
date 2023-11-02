c6e3a3627e8468195ce334926d799c434e12d337
TheBoss2004 <147054361+TheBoss2004@users.noreply.github.com>
2023-11-02 13:07:23 +0100

    test 1

--- docs/script.js
+++ docs/script.js
@@ -1,44 +1,8 @@
-function setup() {
-    createCanvas(400, 400, WEBGL);
-    // This enables device orientation for your application.
-    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-       DeviceOrientationEvent.requestPermission()
-         .then(response => {
-           if (response === 'granted') {
-             window.addEventListener('deviceorientation', deviceOrientationHandler, true);
-           }
-         })
-         .catch(console.error);
-    } else {
-       window.addEventListener('deviceorientation', deviceOrientationHandler, true);
-    }
-   }
+window.addEventListener('deviceorientation', function(event) {
+    var x = event.beta; // In degree in the range of [-180,180]
+    var y = event.gamma; // In degree in the range of [-90,90]
+    var z = event.alpha;
+    var ball = document.getElementById('ball');
+    ball.style.transform = 'translate(' + x + 'px, ' + y + 'px,)';
+   });
    
-   function draw() {
-    background(255);
-    translate(-width / 2, -height / 2);
-   
-    rotateX(ball.roll * PI / 180);
-    rotateY(ball.pitch * PI / 180);
-    rotateZ(ball.yaw * PI / 180);
-   
-    noStroke();
-    fill(0, 0, 255);
-    sphere(100);
-   }
-   
-   let ball = {
-    roll: 0,
-    pitch: 0,
-    yaw: 0
-   };
-   
-   function deviceOrientationHandler(event) {
-    if (event.beta !== null && event.gamma !== null) {
-      ball.roll = event.beta;
-      ball.pitch = event.gamma;
-    }
-    if (event.alpha !== null) {
-      ball.yaw = event.alpha;
-    }
-   }
