16569204be185ad9c1c73a6d132fc4ff10498bd0
TheBoss2004 <147054361+TheBoss2004@users.noreply.github.com>
2023-11-02 12:22:28 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('deviceorientation', function(event) {
  var x = event.beta; // In degree in the range of [-180,180]
  var y = event.gamma; // In degree in the range of [-90,90]
-
+ var z = event.alpha;
  var ball = document.getElementById('ball');
- ball.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+ ball.style.transform = 'translate(' + y + 'px, ' + x + 'px, )';
 });
