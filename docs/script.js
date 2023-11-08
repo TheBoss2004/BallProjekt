566a54861398f2958fa3d052edb735f7d52d4d92
TheBoss2004 <147054361+TheBoss2004@users.noreply.github.com>
2023-11-08 09:22:15 +0100

    check

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,36 @@
-window.addEventListener('deviceorientation', function(event) {
-    var x = event.beta; // In degree in the range of [-180,180]
-    var y = event.gamma; // In degree in the range of [-90,90]
-    var z = event.alpha;
-    var ball = document.getElementById('ball');
-    ball.style.transform = 'translate(' + x + 'px, ' + y + 'px,)';
-   });
-   
+class DeviceOrientationBall {
+    constructor() {
+        this.x = window.innerWidth / 2;
+        this.y = window.innerHeight / 2;
+        this.size = 200;
+    }
+
+    display() {
+        const ball = document.getElementById("device-orientation-ball");
+        ball.style.left = this.x + "px";
+        ball.style.top = this.y + "px";
+    }
+
+    update(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let ball;
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    ball = new DeviceOrientationBall();
+}
+
+function draw() {
+    background(0);
+    ball.display();
+}
+
+function deviceMoved() {
+    const x = map(rotationY, -90, 90, 0, window.innerWidth);
+    const y = map(rotationX, -90, 90, 0, window.innerHeight);
+    ball.update(x, y);
+}
