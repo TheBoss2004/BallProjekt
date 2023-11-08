030fd09a57a815791f86036d04f6f3549fa83aad
TheBoss2004 <147054361+TheBoss2004@users.noreply.github.com>
2023-11-08 20:00:03 +0100

    Hello

--- docs/script.js
+++ docs/script.js
@@ -2,54 +2,57 @@ let ball;
 let alpha, beta, gamma; // Device orientation data
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ball = new Ball(width / 2, height / 2, 50);
-  ball.setSpeed(5);
+ createCanvas(windowWidth, windowHeight);
+ ball = new Ball(width / 2, height / 2, 50);
+ ball.setSpeed(5);
+
+ // Add a listener for the DeviceOrientationAbsoluteEvent
+ window.addEventListener('deviceorientationabsolute', deviceOrientationAbsolute);
 }
 
 function draw() {
-  background(220);
+ background(220);
 
-  // Display the ball
-  ball.display();
+ // Display the ball
+ ball.display();
 
-  // Update ball's position based on device orientation
-  ball.update(beta, gamma);
+ // Update ball's position based on device orientation
+ ball.update(beta, gamma);
 
-  // Check if the ball is out of the canvas
-  if (ball.isOutOfBounds()) {
+ // Check if the ball is out of the canvas
+ if (ball.isOutOfBounds()) {
     // Vibrate the device for 100 milliseconds
     navigator.vibrate(100);
-  }
+ }
 }
 
-function deviceOrientation(event) {
-  alpha = event.alpha;
-  beta = event.beta;
-  gamma = event.gamma;
+function deviceOrientationAbsolute(event) {
+ alpha = event.alpha;
+ beta = event.beta;
+ gamma = event.gamma;
 }
 
 class Ball {
-  constructor(x, y, size) {
+ constructor(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
     this.speedX = 0;
     this.speedY = 0;
-  }
+ }
 
-  setSpeed(speed) {
+ setSpeed(speed) {
     this.speedX = speed;
     this.speedY = speed;
-  }
+ }
 
-  display() {
+ display() {
     fill(0);
     noStroke();
     ellipse(this.x, this.y, this.size);
-  }
+ }
 
-  update(rotationX, rotationY) {
+ update(rotationX, rotationY) {
     // Map the device orientation data to ball movement
     let mappedSpeedX = map(rotationY, -90, 90, -this.speedX, this.speedX);
     let mappedSpeedY = map(rotationX, -90, 90, -this.speedY, this.speedY);
@@ -60,9 +63,9 @@ class Ball {
     // Keep the ball within the canvas screen
     this.x = constrain(this.x, this.size / 2, width - this.size / 2);
     this.y = constrain(this.y, this.size / 2, height - this.size / 2);
-  }
+ }
 
-  isOutOfBounds() {
+ isOutOfBounds() {
     return this.x < this.size / 2 || this.x > width - this.size / 2 || this.y < this.size / 2 || this.y > height - this.size / 2;
-  }
+ }
 }
